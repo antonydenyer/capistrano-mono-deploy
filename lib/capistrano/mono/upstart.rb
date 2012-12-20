6a666aa4f116c1f59f340046819c2a86f4d16970
Antony Denyer <antonydenyer@gmail.com>
2012-12-20 12:42:54 +0000

    upstart 1

---
+++ lib/capistrano/mono/upstart.rb
@@ -0,0 +1,109 @@
+require 'capistrano/configuration/actions/invocation'
+require 'capistrano/configuration/variables'
+
+module Capistrano
+	module Deploy
+		module MONO
+			class Upstart
+				# Ideas taken from https://github.com/loopj/capistrano-node-deploy/blob/master/lib/capistrano/node-deploy.rb
+
+				def upstart_job_name
+					@configuration.variables[:upstart_job_name] || "#{application}-#{node_env}"
+				end
+
+				def upstart_file_path
+					@configuration.variables[:upstart_file_path] || "/etc/init/#{upstart_job_name}.conf"
+				end
+
+				def socket
+					socketName = @configuration.variables[:application] || "fastcgi-mono-server4"
+					socketName = socketName.gsub(/\s+/, "") 
+					"/tmp/SOCK-#{socketName}"
+        		end
+
+        		def latest_release
+        			@configuration.variables[:latest_release]
+        		end
+
+        		def fastcgi_command
+        			@configuration.variables[:fastcgi_command] || "fastcgi-mono-server4 /applications=/:#{latest_release} /filename=#{socket} /socket=unix > /dev/null 2>&1 &"
+        		end
+
+
+				def remote_file_exists?(full_path)
+				  'true' == @configuration.capture("if [ -e #{full_path} ]; then echo 'true'; fi").strip
+				end
+
+				def remote_file_content_same_as?(full_path, content)
+				  Digest::MD5.hexdigest(content) == capture("md5sum #{full_path} | awk '{ print $1 }'").strip
+				end
+
+				def remote_file_differs?(full_path, content)
+				  exists = remote_file_exists?(full_path)
+				  !exists || exists && !remote_file_content_same_as?(full_path, content)
+				end
+
+				def create_upstart_config do
+      				config_file_path = "/etc/init/#{application}.conf"
+      				temp_config_file_path = "#{shared_path}/#{application}.conf"
+      				# Generate and upload the upstart script
+      				put generate_upstart_config, temp_config_file_path
+				     # Copy the script into place and make executable
+      				@configuration.run('sudo "cp #{temp_config_file_path} #{upstart_file_path}"')
+    			end
+				
+				def initialize(configuration={})
+          			@configuration = configuration
+        		end
+
+				def start()
+					create_upstart_config if remote_file_differs?(upstart_file_path, generate_upstart_config)
+					puts "starting application using fastcgi on socket #{socket}"
+
+					@configuration.run("")
+					
+					attempt_number = 0
+					begin
+						attempt_number = attempt_number + 1
+						@configuration.run("curl localhost --silent --location --header 'Host: #{socket}' --output /dev/null")
+					rescue
+						sleep 1
+						retry if attempt_number < 5
+						puts 'You need to configure your web server for fastcgi'
+						puts 'For example in nginx it you need to add the following params:'
+						puts ''
+						puts 'fastcgi_pass unix:/tmp/SOCK-$http_host;'
+						puts 'fastcgi_param  PATH_INFO          "";'
+						puts 'fastcgi_param  SCRIPT_FILENAME    $document_root$fastcgi_script_name;'
+						puts ''
+						raise CommandError.new("Failed to bring up the site")
+					end
+					puts "The site is up"
+				end   
+
+				def stop()
+					@configuration.run("pkill fastcgi-mono-server4 > /dev/null 2>&1 &")
+				end   		
+
+
+				def generate_upstart_config
+					template = '#!upstart ' <<
+						'description "{{application}} node app"' <<
+						'author "capistrano"' <<
+						'' <<
+						'start on (filesystem and net-device-up IFACE=lo)' <<
+						'stop on shutdown' <<
+						'' <<
+						'respawn' <<
+						'respawn limit 99 5'<<
+						'' <<
+						'script' <<
+						'{{fastcgi_command}}' <<
+						'end script'
+  					template.gsub(/\{\{(.*?)\}\}/) { eval($1) }
+				end	
+	
+			end
+		end
+	end
+end
